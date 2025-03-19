ceba204b6e4efabb515fe00bd2ac24da4fadb1ea
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-03-19 12:21:41 +0300

    рекурсия вниз

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,16 +1,17 @@
 ﻿
-let rec sumOfDigits n =
-    if n = 0 then
-        0 
-    else
-        (n % 10) + sumOfDigits (n / 10) 
+
+// Рекурсия вниз (с телом-выражением)
+let rec sumOfDigitsDown n =
+    match n with
+    | 0 -> 0 // Базовый случай
+    | _ -> sumOfDigitsDown (n / 10) + (n % 10) // Рекурсивный случай
 
 
 let main () =
     printfn "Введите число:"
     let number = System.Console.ReadLine() |> int 
 
-    let sum = sumOfDigits number 
+    let sum = sumOfDigitsDown number 
     printfn "Сумма цифр числа %d равна %d" number sum 
 
 main()
