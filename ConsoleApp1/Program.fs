411e2ddae5263c4257b44d12e0665aef0459a493
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-03-19 12:17:05 +0300

    рекурсия вверх

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,44 +1,16 @@
-﻿printfn "Hello World"
-let solve (a,b,c) =
-     let D = b*b-4.*a*c
-     ((-b+sqrt(D))/(2.*a), (-b-sqrt(D))/(2.*a)) in
-         solve (1.0, 2.0,-3.0);;
+﻿
+let rec sumOfDigits n =
+    if n = 0 then
+        0 
+    else
+        (n % 10) + sumOfDigits (n / 10) 
 
-
-
-let circleArea r = 
-    let pi =3.14159
-    pi *r * r
-
-let cylindrevolume r h =
-    h * circleArea r
-
-let bigCylinderVol = cylindrevolume 10
 
 let main () =
-    // суперпозиция
-    printfn "Введите радиус цилиндра:"
-    let radius = System.Console.ReadLine() |> float
-    printfn "Введите высоту цилиндра:"
-    let height = System.Console.ReadLine() |> float
-    let calculateVolume = circleArea >> ((*) height)
-    let volume = calculateVolume radius
+    printfn "Введите число:"
+    let number = System.Console.ReadLine() |> int 
 
-    printfn "Объем цилиндра (суперпозиция): %f" volume
-
-    // каррирование
-    printfn "Введите радиус цилиндра:"
-    let radius = System.Console.ReadLine() |> float
-
-    printfn "Введите высоту цилиндра:"
-    let height = System.Console.ReadLine() |> float
-
-    let calculateVolume = cylindrevolume radius 
-    let volume = calculateVolume height
-
-    printfn "Объем цилиндра (каррирование): %f" volume
-
-    System.Console.ReadKey()
-
+    let sum = sumOfDigits number 
+    printfn "Сумма цифр числа %d равна %d" number sum 
 
 main()
