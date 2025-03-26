a6f4506374badaecb16def7fbc0ad119514d57db
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-03-26 13:08:54 +0300

    Лаб1 задание 15

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -3,24 +3,24 @@
     | 0 -> a
     | _ -> gcd b (a % b)
 
-let traverseCoprimes number operation initial =
+let traverseCoprimesWithCond number operation initial cond =
     let rec loop current acc =
         match current <= 1 with
         | true -> acc
         | false ->
             let isCoprime = gcd current number = 1
+            let meetsCond = cond current
             let newAcc = 
-                match isCoprime with
+                match isCoprime && meetsCond with
                 | true -> operation acc current
                 | false -> acc
             loop (current - 1) newAcc
     loop number initial
 
-let eulerPhi n = 
-    traverseCoprimes n (fun acc _ -> acc + 1) 0
+let sumEvenCoprimes n =
+    traverseCoprimesWithCond n (+) 0 (fun x -> x % 2 = 0)
 
-let testCoprimes () =
-    printfn "Сумма взаимно простых с 10: %d" (traverseCoprimes 10 (+) 0)
-    printfn "φ(10) = %d" (eulerPhi 10)
+let testCondCoprimes () =
+    printfn "Сумма чётных взаимно простых с 10: %d" (sumEvenCoprimes 10)
 
-testCoprimes()
+testCondCoprimes()
