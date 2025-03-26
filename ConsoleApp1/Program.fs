b9c26660eb21796b1f8e61b0320c0ec31af9d960
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-03-26 12:55:56 +0300

    Лаб 1 11 задание

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,27 +1,10 @@
-﻿let conditionalDigitTraversal number operation initial condition =
-    let rec loop n acc =
-        match n with
-        | 0 -> acc
-        | _ ->
-            let digit = n % 10
-            let newAcc = 
-                match condition digit with
-                | true -> operation acc digit
-                | false -> acc
-            loop (n / 10) newAcc
-    loop number initial
+﻿let reactToLanguage language =
+    match language with
+    | "F#" | "Prolog" -> "Ты — подлиза!"
+    | "Python" -> "Неплохой выбор, но где функциональщина?"
+    | "Java" -> "ООП — это серьёзно!"
+    | "C++" -> "Сложно, но интересно!"
+    | _ -> "Хороший вкус!"
 
-let testConditionalDigitTraversal () =
-    let number = 123456789
-    
-    let sumEven = conditionalDigitTraversal number (+) 0 (fun x -> x % 2 = 0)
-    printfn "Сумма четных цифр числа %d: %d" number sumEven
-    
-    let productGreaterThan5 = conditionalDigitTraversal number (*) 1 (fun x -> x > 5)
-    printfn "Произведение цифр числа %d больше 5: %d" number productGreaterThan5
-    
-    let maxOdd = conditionalDigitTraversal number max System.Int32.MinValue (fun x -> x % 2 <> 0)
-    printfn "Максимальная нечетная цифра числа %d: %d" number maxOdd
-
-
-testConditionalDigitTraversal()
+// Пример вызова:
+printfn "%s" (reactToLanguage "F#")  // Выведет: "Ты — подлиза!"
