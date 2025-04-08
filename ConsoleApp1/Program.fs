c8eda963c464742a781679bb1fa817754dcd5fa8
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-04-08 21:48:23 +0300

    Lab7.3 Parse

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,150 +1,37 @@
-﻿
-open System
-(*
-[<AbstractClass>]
-type GeometricFigure() =
-    abstract member CalculateArea: unit -> float
-    override this.ToString() = 
-        sprintf "Площадь: %.2f" (this.CalculateArea())
+﻿open FParsec
 
-type IPrint =
-    abstract member Print: unit -> unit
+type Expr =
+    | Num of int
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
 
-type Rectangle(width: float, height: float) =
-    inherit GeometricFigure()
+let numParser: Parser<Expr, unit> =
+    pint32 |>> fun n -> Num n
 
-    member val Width = width with get, set
-    member val Height = height with get, set
+let addParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '+' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Add (left, right)
 
-    override this.CalculateArea() =
-        this.Width * this.Height
+let subParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '-' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Sub (left, right)
 
-    override this.ToString() =
-        sprintf "Прямоугольник: Ширина = %.2f, Высота = %.2f, Площадь = %.2f" this.Width this.Height (this.CalculateArea())
+let exprParser: Parser<Expr, unit> =
+    addParser <|> subParser <|> numParser
 
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
+let parseExpression (input: string) =
+    match run exprParser input with
+    | Success(result, _, _) -> Some result
+    | Failure(msg, _, _) -> None
 
-type Square(sideLength: float) =
-    inherit Rectangle(sideLength, sideLength)
+let input1 = "3 + 4"
+let input2 = "10 - 3"
+let input3 = "5"
 
-    override this.ToString() =
-        sprintf "Квадрат: Сторона = %.2f, Площадь = %.2f" this.Width (this.CalculateArea())
+let result1 = parseExpression input1
+let result2 = parseExpression input2
+let result3 = parseExpression input3
 
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
-
-type Circle(radius: float) =
-    inherit GeometricFigure()
-
-    member val Radius = radius with get, set
-
-    override this.CalculateArea() =
-        Math.PI * this.Radius * this.Radius
-
-    override this.ToString() =
-        sprintf "Круг: Радиус = %.2f, Площадь = %.2f" this.Radius (this.CalculateArea())
-
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
-
-type GeometricFigureType =
-    | Rectangle of width: float * height: float
-    | Square of sideLength: float
-    | Circle of radius: float
-
-let calculateArea figure =
-    match figure with
-    | Rectangle (width, height) -> width * height
-    | Square (sideLength) -> sideLength * sideLength
-    | Circle (radius) -> Math.PI * radius * radius
-
-let printFigure figure =
-    match figure with
-    | Rectangle (width, height) ->
-        printfn "Прямоугольник: Ширина = %.2f, Высота = %.2f, Площадь = %.2f" width height (calculateArea figure)
-    | Square (sideLength) ->
-        printfn "Квадрат: Сторона = %.2f, Площадь = %.2f" sideLength (calculateArea figure)
-    | Circle (radius) ->
-        printfn "Круг: Радиус = %.2f, Площадь = %.2f" radius (calculateArea figure)
-
-let rectangle = Rectangle(5.0, 10.0)
-let square = Square(4.0)
-let circle = Circle(3.0)
-
-printFigure rectangle
-printFigure square
-printFigure circle
-
-let fig1 = Rectangle(5.0, 10.0)
-let fig2 = Square(4.0)
-let fig3 = Circle(3.0)
-
-printfn "Площадь прямоугольника: %.2f" (calculateArea fig1)
-printfn "Площадь квадрата: %.2f" (calculateArea fig2)
-printfn "Площадь круга: %.2f" (calculateArea fig3)
-*)
-
-open System
-
-// Определяем тип Maybe, который может быть Just или Nothing
-type Maybe<'a> =
-    | Just of 'a
-    | Nothing
-
-// Реализация модуля для функтора
-module Functor =
-    let map f maybe =
-        match maybe with
-        | Just x -> Just (f x)
-        | Nothing -> Nothing
-
-// Реализация модуля для аппликативного функтора
-module Applicative =
-    let pure x = Just x
-
-    let apply f maybe =
-        match f, maybe with
-        | Just f', Just x -> Just (f' x)
-        | _ -> Nothing
-
-// Реализация модуля для монады
-module Monad =
-    let bind maybe f =
-        match maybe with
-        | Just x -> f x
-        | Nothing -> Nothing
-
-    let returnMaybe x = Just x
-
-// Определение функций для тестирования
-let increment x = x + 1
-let double x = x * 2
-
-// Реализация ToString для типа Maybe
-let maybeToString (maybe: Maybe<'a>) =
-    match maybe with
-    | Just x -> sprintf "Just %A" x
-    | Nothing -> "Nothing"
-
-// Применение операцій для тестирования
-let resultFunctor = Functor.map increment (Just 5)
-let resultNothingFunctor = Functor.map increment Nothing
-
-let resultApplicative = Applicative.apply (Just increment) (Just 5)
-let resultNothingApplicative = Applicative.apply (Just increment) Nothing
-
-let resultMonad = Monad.bind (Just 5) (fun x -> Just (x * 2))
-let resultNothingMonad = Monad.bind Nothing (fun x -> Just (x * 2))
-
-// Вывод результатов с помощью функции maybeToString
-Console.WriteLine("Результат функтора: " + maybeToString resultFunctor)
-Console.WriteLine("Результат функтора для Nothing: " + maybeToString resultNothingFunctor)
-Console.WriteLine("Результат аппликативного функтора: " + maybeToString resultApplicative)
-Console.WriteLine("Результат аппликативного функтора для Nothing: " + maybeToString resultNothingApplicative)
-Console.WriteLine("Результат монады: " + maybeToString resultMonad)
-Console.WriteLine("Результат монады для Nothing: " + maybeToString resultNothingMonad)
- 
+printfn "Результат 1: %A" result1
+printfn "Результат 2: %A" result2
+printfn "Результат 3: %A" result3
