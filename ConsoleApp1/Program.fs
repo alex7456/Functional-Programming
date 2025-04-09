ec1ea1d46b647562d034c777ad7fedc708bd625c
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-04-09 18:03:45 +0300

    lab 7 bazhenov)))

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,12 +1,10 @@
 ﻿open System
 
-// Определяем типы сообщений
 type Message =
     | PrintMessage of string
     | DoubleNumber of int
     | Stop
 
-// Создаем агента
 let agent = MailboxProcessor.Start(fun inbox ->
     let rec loop () =
         async {
@@ -19,16 +17,16 @@ let agent = MailboxProcessor.Start(fun inbox ->
                 printfn "Число удвоено: %d" (num * 2)
             | Stop ->
                 printfn "Агент завершает работу."
-                return ()  // Завершаем выполнение
-            return! loop ()  // Возвращаем к рекурсии для ожидания сообщений
+                return ()  
+            return! loop () 
         }
-    loop ())  // Начинаем цикл обработки сообщений
+    loop ())  
 
-// Отправка сообщений в агент
-agent.Post(PrintMessage "Hello, world!")  // Сообщение для печати
-agent.Post(DoubleNumber 5)                // Сообщение для удвоения числа
-agent.Post(DoubleNumber 10)               // Сообщение для удвоения числа
-agent.Post(Stop)                          // Сообщение для завершения работы агента
 
-// Ожидаем ввода, чтобы не закрыть консоль сразу
+agent.Post(PrintMessage "Hello, world!")  
+agent.Post(DoubleNumber 5)                
+agent.Post(DoubleNumber 10)               
+agent.Post(Stop)                          
+
+
 Console.ReadLine() 
--- Passport/Program.fs
+++ Passport/Program.fs
@@ -5,30 +5,24 @@
 open System
 open System.Text.RegularExpressions
 
-// Класс для паспорта гражданина РФ
 type Passport(seria: string, number: string, fullName: string, issueDate: DateTime, issuedBy: string) =
-    // Поля класса
     let mutable _seria = seria
     let mutable _number = number
     let mutable _fullName = fullName
     let mutable _issueDate = issueDate
     let mutable _issuedBy = issuedBy
 
-    // Валидация серии и номера паспорта с использованием регулярных выражений
     member this.ValidateSeria() =
-        let seriaPattern = @"^\d{4}$" // Серия должна быть 4 цифры
-        // Явно приводим _seria к строке, чтобы избежать ошибок компиляции
+        let seriaPattern = @"^\d{4}$" 
         Regex.IsMatch(string _seria, seriaPattern)
 
     member this.ValidateNumber() =
-        let numberPattern = @"^\d{6}$" // Номер должен быть 6 цифр
-        // Явно приводим _number к строке
+        let numberPattern = @"^\d{6}$" 
         Regex.IsMatch(string _number, numberPattern)
 
     member this.Validate() =
         this.ValidateSeria() && this.ValidateNumber()
 
-    // Свойства
     member this.Seria 
         with get() = _seria
         and set(value) =
@@ -53,7 +47,6 @@ type Passport(seria: string, number: string, fullName: string, issueDate: DateTi
         with get() = _issuedBy
         and set(value) = _issuedBy <- value
 
-    // Метод для вывода документа на экран
     member this.PrintDocument() =
         if this.Validate() then
             printfn "Паспорт гражданина РФ:"
@@ -65,19 +58,15 @@ type Passport(seria: string, number: string, fullName: string, issueDate: DateTi
         else
             printfn "Ошибка: Неверный формат данных паспорта."
 
-    // Метод для сравнения документов
     member this.Equals(other: Passport) =
         this.Seria = other.Seria && this.Number = other.Number
 
-// Пример использования
 let passport1 = new Passport("1234", "567890", "Иванов Иван Иванович", DateTime(2020, 5, 10), "ОВД Ленина")
 let passport2 = new Passport("1234", "567890", "Петров Петр Петрович", DateTime(2021, 6, 15), "ОВД Центральный")
 
-// Печать документов
 passport1.PrintDocument()
 passport2.PrintDocument()
 
-// Сравнение документов
 if passport1.Equals(passport2) then
     printfn "Документы одинаковые."
 else
